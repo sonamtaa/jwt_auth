cc3055264ff05d3ea0de9df5788aead01e7e52bd
sonamtaa <sonam.tashi@selise.ch>
2022-09-08 17:11:15 +0600

    test: add registraction sepc now for create

--- spec/requests/users/registration_spec.rb
+++ spec/requests/users/registration_spec.rb
@@ -2,68 +2,42 @@
 
 require 'rails_helper'
 
-describe 'User registration' do
+describe 'User Registration' do
   let!(:params) do
     {
       user: {
+        name: 'Sonam tashi',
+        username: 'sonataa',
         email: 'st+user@gmail.com',
-        role_id: Role.second.id,
-        active: true,
-        profile_attributes: {
-          first_name: 'Sonam',
-          last_name: 'Tashi',
-          phone: '+97517524816',
-          salutation: 'Mr',
-          designation: 'Developer',
-          image_url: Faker::LoremPixel.image,
-          user_address_attributes: {
-            street_name: 'Erdman Wall',
-            street_no: '137',
-            city: 'Lake Beverlymouth',
-            zip: '4051',
-            state: 'Iowa',
-            country: 'Iceland',
-            description: 'cold place',
-            additional_address: 'Near town',
-            post_box_no: '2'
-          }
-        }
+        password: 'Test20',
+        password_confirmation: 'Test20'
       }
     }
   end
+
   let!(:invalid_params) do
     {
       user: {
-        email: 'Testing',
-        role_id: 2,
-        active: false
+        name: 'Sonam tashi',
+        username: 'sonataa',
+        password: 'Test20',
+        password_confirmation: 'Test20',
+        email: 'Testing'
       }
     }
   end
 
   context 'with valid params' do
     it 'registers the user' do
-      post user_registration_path, params: params, headers: {}
-      expect(status).to eq(200)
-      expect(json.dig(:user, :email)).to eq('st+user@gmail.com')
-    end
-  end
-
-  context 'with auto approve' do
-    let!(:user) { create(:user) }
-
-    it 'auto approves user when user registers' do
-      post user_registration_path, params: params, headers: {}
-      expect(status).to eq(200)
-      user = User.find(json.dig('user', 'id'))
-      expect(user.status).to eq('approved')
-      expect(user.active).to be(true)
+      post users_path, params: params, headers: {}
+      expect(status).to eq(201)
+      expect(json[:email]).to eq('st+user@gmail.com')
     end
   end
 
   context 'with invalid params' do
     it 'does not registers the user' do
-      post user_registration_path, params: invalid_params, headers: {}
+      post users_path, params: invalid_params, headers: {}
       expect(status).to eq(422)
     end
   end
